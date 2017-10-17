5550e5bbe3cbd6116c548157c07ecc2b2a5a759a
eddymorra <thibaut.cout@gmail.com>
2017-10-17 11:38:50 +0200

    Essai de test sur product.component :(

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement }    from '@angular/core';
 
 import { ProductComponent } from './product.component';
+import { ProductService } from './product.service';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
+  let debug: DebugElement;
+  let htmlElem: HTMLElement;
 
   beforeEach(async(() => {
+    //_productServiceStub = {};
+
     TestBed.configureTestingModule({
-      declarations: [ ProductComponent ]
+      declarations: [ ProductComponent ],
+      //providers: [ {provide: ProductService, useValue: _productServiceStub} ]
+      providers: [ ProductService ]
     })
     .compileComponents();
   }));
@@ -17,6 +25,8 @@ describe('ProductComponent', () => {
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    //_productServiceStub = TestBed.get(ProductService)
   });
 
   it('should create', () => {
